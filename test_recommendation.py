0153d6088db411935c911fa93d23a92a89e6e3ba
saichalla73 <140317790+saichalla73@users.noreply.github.com>
2023-11-26 17:12:36 +0530

    test_recommendation.py

---
+++ test_recommendation.py
@@ -0,0 +1,27 @@
+## Sample Input1:Italian
+## Sample Output1:Enter the type of food you desire: Recommended restaurants:
+##                Name: Little Italy, Cuisine: Italian, Rating: 4.4, Location: Jubilee Hills
+##                Name: Prego, Cuisine: Italian, Rating: 4.3, Location: Madhapur
+
+## Sample Input2:gulabjam
+## Sample Output2:Enter the type of food you desire: Sorry, the entered type of food is unavailable or invalid.
+
+## Sample Input3:Seafood, Goan
+## Sample Output3:Enter the type of food you desire: Recommended restaurants:
+##                Name: The Fisherman's Wharf, Cuisine: Seafood, Goan, Rating: 4.2, Location: Gachibowli
+
+## Sample Input4:Indian, Chinese
+## Sample Output4:Enter the type of food you desire: Recommended restaurants:
+##                Name: Ohri's Jiva Imperia, Cuisine: Indian, Chinese, Rating: 4.1, Location: Necklace Road
+
+## Sample Input5:Hyderabadi, Biryani
+## Sample Output5:Enter the type of food you desire: Recommended restaurants:
+##                Name: Café Bahar, Cuisine: Hyderabadi, Biryani, Rating: 4.3, Location: Basheer Bagh
+##                Name: Paradise - Food Court, Inorbit Mall, Cuisine: Hyderabadi, Biryani, Rating: 4.1, Location: Hitech City
+
+## Sample Input6:Rajasthani, Gujarati
+## Sample Output6:Enter the type of food you desire: Recommended restaurants:
+##                Name: Rajdhani Thali Restaurant, Cuisine: Rajasthani, Gujarati, Rating: 4.2, Location: Kothapet
+
+## Sample Input7:12451
+## Sample Output7:Enter the type of food you desire: Sorry, the entered type of food is unavailable or invalid.
